63c61dff7cd528a7e8afe774c82427983e2d070f
Sodany <sodany@Air-de-Sodany.lan>
2025-04-06 15:06:50 +0200

    first commit on dev

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
 ];

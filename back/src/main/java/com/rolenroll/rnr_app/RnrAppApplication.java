5b98483b17c40f0f5854e95650bfedeba90f5ea0
Sodany <sodany@Air-de-Sodany.lan>
2025-04-07 19:53:44 +0200

    feat: path helllo working

---
+++ back/src/main/java/com/rolenroll/rnr_app/RnrAppApplication.java
@@ -0,0 +1,13 @@
+package com.rolenroll.rnr_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnrAppApplication.class, args);
+	}
+
+}

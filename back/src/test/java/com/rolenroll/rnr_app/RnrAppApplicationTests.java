946ff85ae3c843d7d21ac81e37ecb1b3c7340d76
Sodany SEAN <champloo63@gmail.com>
2025-09-02 16:36:31 +0200

    profile added

--- back/src/test/java/com/rolenroll/rnr_app/RnrAppApplicationTests.java
+++ back/src/test/java/com/rolenroll/rnr_app/RnrAppApplicationTests.java
@@ -3,7 +3,12 @@ package com.rolenroll.rnr_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+
 class RnrAppApplicationTests {
 
 	@Test
